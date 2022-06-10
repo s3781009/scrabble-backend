c5c3ca13c72a40ea92b88f9624ffb11b3da9e0cb
s3781009 <s3781009@student.rmit.edu.au>
2022-06-10 23:20:20 +1000

    first commit

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func setupRoutes() {
+
+	http.HandleFunc("/play", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "Hello!")
+		if err != nil {
+			fmt.Println(err)
+		}
+	})
+
+	http.HandleFunc("/new", func(w http.ResponseWriter, r *http.Request) {
+		upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+		ws, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println(err)
+		}
+		log.Println("succesfully connected ...")
+		newWsConnection(ws)
+	})
+}
+
+//reads the http request to sends back the game code /
+func newWsConnection(conn *websocket.Conn) {
+
+	for {
+		gameId := 0
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		gameId++
+		log.Println(string(p))
+		if err := conn.WriteMessage(messageType, []byte(strconv.Itoa(gameId))); err != nil {
+
+			log.Println(err)
+		}
+	}
+
+}
+
+func main() {
+
+	setupRoutes()
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
