a81473935cbd5c912be7d39a14970f7f0cf023b7
s3781009 <s3781009@student.rmit.edu.au>
2022-06-12 23:50:07 +1000

    fix

--- main.go
+++ main.go
@@ -204,7 +204,7 @@ func main() {
 	setupRoutes()
 	fmt.Println("Starting server at port at :" + os.Getenv("PORT"))
 	srv := http.Server{
-		Addr:         ":8080",
+		Addr:         ":" + os.Getenv("PORT"),
 		WriteTimeout: 1 * time.Minute,
 		ReadTimeout:  1 * time.Minute,
 	}
